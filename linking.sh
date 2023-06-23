60d0119ab9fa66b7aa055cbfcd3d58d9368c9257
Daniel Araya <darayaroma@gmail.com>
2023-06-22 19:47:12 -0600

    [script for my future self]

---
+++ linking.sh
@@ -0,0 +1,6 @@
+# linking my dotfiles
+# (shell, terminal, text-editor)
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.config/alacritty ~/.config/alacritty
+ln -s ~/dotfiles/.config/nvim ~/.config/nvim
+
