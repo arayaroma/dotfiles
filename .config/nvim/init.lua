7f5a7f35c521c2400e8cd11f482d53d88defc022
Daniel Araya <darayaroma@gmail.com>
2023-06-22 19:41:28 -0600

    [Added nvim config]

---
+++ .config/nvim/init.lua
@@ -0,0 +1,9 @@
+
+-- settings
+require('core.settings')
+
+-- keymaps
+require('core.keymaps')
+
+-- plugins
+require('core.plugins')
---
+++ .config/nvim/lua/core/settings.lua
@@ -0,0 +1,21 @@
+local opt           =   vim.opt
+
+opt.number          =	true
+opt.relativenumber  =	true
+opt.autoindent      =	true
+opt.ruler           =	true
+opt.showcmd         =   true
+opt.smartcase       =	true
+opt.smartindent     =   true
+opt.splitbelow      =   true
+opt.splitright      =   true
+opt.termguicolors   =   true
+opt.expandtab       =	true
+opt.shiftround      =   true
+opt.cursorline      =   true
+opt.autowrite       =   true
+opt.autoread        =   true
+
+opt.tabstop         =	4
+opt.softtabstop     =   4
+opt.shiftwidth      =   4
